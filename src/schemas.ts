f243c0e990aa80d9c3eafb7d60fbf6b6a1b644a0
ismailkhandokar0 <ismailkhandokar0@gmail.com>
2026-03-27 22:42:38 +0600

    project intialized

---
+++ src/schemas.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const CreateTodoSchema = z.object({
+    taskName: z.string().min(1),
+    description: z.string().optional(),
+    dueDate: z.string(),
+    category: z.string(),
+})
+
+export const UpdateTodoSchema = z.object({
+    id: z.number(),
+    taskName: z.string().min(1),
+    description: z.string().optional(),
+    dueDate: z.string(),
+    category: z.string(),
+})
+
+export const DeleteTodoSchema = z.object({
+    id: z.number(),
+})
